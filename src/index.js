822cf075107e9c608eb96b5067b7eb6d82dfbb9a
ivanov9826 <ivanov9826@gmail.com>
2022-06-20 19:58:11 +0300

    Render home page & move static to root folder
    move static to root folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const app = express();
 
+app.use('/static' , express.static('static'));
+
+app.engine('hbs' , hbs.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine' , 'hbs');
+app.set('views' , './src/views');
+
 app.get('/' , (req , res) =>{
-    res.send('Hello world');
+    res.render('index')
 });
 
 app.listen(3000 , () => console.log(`App is listening on port 3000`));
