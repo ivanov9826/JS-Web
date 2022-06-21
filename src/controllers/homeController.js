d09168ef3c5afa4670fdb0450dd91f503fd36ffa
ivanov9826 <ivanov9826@gmail.com>
2022-06-21 19:45:40 +0300

    Search fix

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,9 +7,9 @@ const cubeService = require('../services/cubeService');
 router.get('/', (req, res) => {
     let {search , from , to} = req.query;
 
-    const cubes = cubeService.getAll(search , from , to);
+    const cubes = cubeService.getAll(search , Number(from) , Number(to));
 
-    res.render('index', { cubes });
+    res.render('index', { cubes , search , from , to});
 });
 
 router.get('/about/', (req, res) => {
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -4,18 +4,21 @@ const cubes = require('../db.json');
 
 const path = require('path');
 
-exports.getAll = (search = '', from = 0 , to = 6) => {
-   const result = cubes
+exports.getAll = (search = '', fromInput, toInput) => {
+    const from = Number(fromInput) || 0;
+    const to = Number(toInput) || 6
+
+    const result = cubes
         .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
         .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to)
 
-   return result;
+    return result;
 };
 
 exports.getOne = (cubeId) => cubes[cubeId]
 
 exports.save = (cube) => {
-    cubes.push({id: cubes[cubes.length - 1].id + 1, ...cube});
+    cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
 
-    return fs.writeFile(path.resolve('src' , 'db.json') , JSON.stringify(cubes , '' , 4)) , {encoding: 'utf-8'};
+    return fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4)), { encoding: 'utf-8' };
 }; 
