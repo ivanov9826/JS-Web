0642c516f97b2aeaa691aa54f21ad8fadd797e75
ivanov9826 <ivanov9826@gmail.com>
2022-06-24 08:42:44 +0300

    Create cube model

---
+++ models/cube.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = mongoose.Schema({
+    _id:mongoose.Types.ObjectId,
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 120
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+    },
+});
+
+cubeSchema.path('imageUrl').validate(function(){
+    return this.name.startsWith('http');
+}, 'Image URL should be a link!');
+
+const Cube = mongoose.model('Cube' , cubeSchema);
+
+module.exports = Cube;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const hbs = require('express-handlebars');
-const routes = require('./routes')
 
+
+const { initializeDatabase } = require('./config/database');
+const routes = require('./routes');
 const app = express();
 
-app.use('/static' , express.static('static'));
+require('./config/handlebars')(app);
 
-app.use(express.urlencoded({extended: false}));
+app.use('/static', express.static('static'));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
 
-app.engine('hbs' , hbs.engine({
-    extname: 'hbs'
-}));
-
-app.set('view engine' , 'hbs');
-app.set('views' , './src/views');
-
-app.use(routes)
-
-
-app.listen(3000 , () => console.log(`App is listening on port 3000`));
+initializeDatabase()
+    .then(() => {
+        app.listen(3000, () => console.log(`App is listening on port 3000`));
+    })
+    .catch((err) => {
+        console.log('Cannot connect to db' , err);
+    });
